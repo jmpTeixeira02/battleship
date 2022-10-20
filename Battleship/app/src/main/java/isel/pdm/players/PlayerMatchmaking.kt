6975e47b3fd1ca7c4acad21e185daec9c2149c6c
a46109 <cravojmls@gmail.com>
2022-10-20 22:26:01 +0100

    adds 3 different player card status

---
+++ Battleship/app/src/main/java/isel/pdm/players/PlayerMatchmaking.kt
@@ -0,0 +1,3 @@
+package isel.pdm.players
+
+data class PlayerMatchmaking(val name: String)
--- Battleship/app/src/main/java/isel/pdm/ui/screen/HomeScreen.kt
+++ Battleship/app/src/main/java/isel/pdm/ui/screen/HomeScreen.kt
@@ -1,16 +1,24 @@
 package isel.pdm.ui.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Info
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import isel.pdm.R
 import isel.pdm.ui.elements.TopBar
 import isel.pdm.ui.theme.BattleshipTheme
+
+/*data class PlayersListScreen(
+    val players: List<PlayerView> = emptyList()
+)*/
 
 @Composable
 fun HomeScreen(
@@ -30,7 +38,13 @@ fun HomeScreen(
                     title = stringResource(id = R.string.app_name)
                 )
             }
-        ){
+        ) { innerPadding ->
+            LazyColumn(contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier.padding(innerPadding)
+            ) {
+                TODO()
+            }
 
         }
     }
@@ -43,4 +57,10 @@ private fun HomeScreenPreview(){
         aboutUsRequest = {},
         replayRequest = {}
     )
+}
+
+@Preview
+@Composable
+private fun PlayerInvitePreview () {
+
 }
