11934e3bbfe4de0701bd5555c86457dc4968fa2e
MaheuTaroo <pantupa@gmail.com>
2022-11-09 14:45:34 +0000

    Real-service-ready functions created, ready to change

--- Battleship/app/src/main/java/isel/pdm/data/game/Replay.kt
+++ Battleship/app/src/main/java/isel/pdm/data/game/Replay.kt
@@ -1,35 +1,78 @@
 package isel.pdm.data.game
 
-data class Replay(val replayId: String, val date: String, val opponentName: String, val turns: MutableList<Turn>) {
+data class Replay(val replayId: String = , val date: String = LocalDate.now().toString(), val opponentName: String, val turns: List<Turn> = listOf()) {
     companion object {
         private var dump: String = ""
 
-        fun dump(rep: Replay) {
+        fun dump(/*path: String, */rep: Replay) {
             try {
                 if (rep.replayId.contains("|||")) throw Exception("replay ID had illegal character sequence")
+
+                // This line tests if the given date is valid; if it is not valid, a DateTimeException
+                // is thrown. Since the main objective is date testing, the return variable can be
+                // discarded, since it will not be used
+                LocalDate.parse(rep.date)
+
                 dump = rep.replayId + "|||" + rep.date + "|||" + rep.opponentName + "\n--------------------"
                 rep.turns.forEach {
                     dump += "\n" + it
                 }
+
+                /*
+                var file = File(path)
+                file.writeText(rep.replayId + "|||" + rep.date + "|||" + rep.opponentName + "\n--------------------")
+                rep.turns.forEach {
+                    file.appendText("\n" + it)
+                }
+                 */
             }
             catch (e: Exception) {
                 throw Exception("Could not mark game as favorite - ${e.message}")
             }
         }
 
-        fun read() : Replay {
+        fun read(/*path: String*/) : Replay {
             try {
-                val id = dump.substring(0, dump.indexOf("|||"))
-                val date = dump.substring(dump.indexOf("|||") + 3, dump.lastIndexOf("|||"))
-                val opponent = dump.substring(dump.lastIndexOf("|||") + 3, dump.IndexOf("\n--------------------\n")
+                val header = readHeaderInfo()
                 val turns = mutableListOf<Turn>()
-                dump.substring(dump.IndexOf("\n--------------------\n" + 22), dump.length + 1).split(Regex.fromLiteral("\n")). forEach {
+                dump.substring(dump.IndexOf("\n--------------------\n") + 22, dump.length + 1).split(Regex.fromLiteral("\n")).forEach {
                     turns.add(Turn.fromString(it))
                 }
-                return Replay(id, date, opponent, turns)
+
+                /*
+                val file = File(path).readLines()
+                val header = file.subList(0, 1).split("|||")
+                val turns = file.subList(2, file.size + 1)
+                 */
+
+                return Replay(header[0], header[1], header[2], turns)
             }
             catch (e: Exception) {
-                throw Exception("Could not replay favorite game - ${e.message}")
+                throw Exception("Could not replay favorite game: ${e.message}")
+            }
+        }
+
+        fun readHeaderInfo(/*path: String*/): List<String> {
+            try {
+                return dump.substring(0, dum.indexOf("\n")).split("|||")
+
+                /*
+                val header = File(path).readLines()[0]
+                return header.split(Regex.fromLiteral("|||"))
+                 */
+            }
+            catch (e: Exception) {
+                throw Exception("Could not retrieve replay info - ${e.message}")
+            }
+
+        }
+
+        fun getTurns(path: String): List<Turn> {
+            var file = File(path)
+            var temp: List<Turn> = file.readLines()
+            var res = mutableListOf<Turn>()
+            temp.subList(2, temp.size + 1).forEach {
+                res.add(Turn.fromString(it.toString()))
             }
         }
     }
